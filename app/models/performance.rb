ed6593ed89600560e894285b3a006916d649a29f
Tim Galeckas <timgaleckas@chromedome-lite.local>
2010-03-21 11:28:00 -0500

    fix: Performance code is mandatory and unique for a production. A performance code should be mandatory and unique for the production.

--- app/models/performance.rb
+++ app/models/performance.rb
@@ -1,6 +1,8 @@
 class Performance < ActiveRecord::Base
   PERFORMANCE_STATUSES = ['Active',  'Inactive']
   validates_inclusion_of :status,        :in => PERFORMANCE_STATUSES
+  validates_uniqueness_of :performance_code, :scope => :production_id
+  validates_presence_of :performance_code
   belongs_to :production
   has_and_belongs_to_many :ticket_classes
   has_many :line_items
