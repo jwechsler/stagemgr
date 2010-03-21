b54e19a0e04214562b44ed6a9182c91a8c71bef1
Tim Galeckas <timgaleckas@chromedome-lite.local>
2010-03-21 09:49:36 -0500

    fix: Ticket prices and fees should show up in currency format on ticket class list

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,7 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def to_currency(val)
+    number_to_currency(val,:delimiter => ",", :unit => "$",:separator => ".")
+  end
 end
+
