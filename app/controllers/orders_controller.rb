65823fa2e64ddf9ede8e4602a398a3a6d8b65e18
Tim Galeckas <timgaleckas@chromedome-lite.local>
2010-04-26 17:27:00 -0500

    monolithic order create now works on public side

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,10 @@
 class OrdersController < ApplicationController
   append_before_filter :find_parents
+  append_before_filter :find_order, :only => [:show, :edit, :update, :destroy]
+  append_before_filter :redirect_to_proper_action, :only => [:edit, :show]
+  
   def new
-    @order = @performance.orders.build(:status=>Order::WEB, :wizard_stage=>1)
+    @order = @performance.orders.build(:status=>Order::WEB)
     @performance.ticket_classes.each{|tc|@order.line_items.build(:ticket_class=>tc)}
 
     respond_to do |format|
@@ -17,12 +20,12 @@ class OrdersController < ApplicationController
 
   def create
     @order = Order.new(params[:order])
+    @order.status = Order::PROCESSING
 
     respond_to do |format|
-      if @order.save
-        @order.wizard_stage = @order.wizard_stage.to_i + 1
+      if @order.save 
         flash[:notice] = 'Order was successfully created.'
-        format.html { render :action=> "new" }
+        format.html { redirect_to(edit_production_performance_order_path(@order.performance.production, @order.performance, @order)) }
         format.xml  { render :xml => @order, :status => :created, :location => @order }
       else
         format.html { render :action => "new" }
@@ -36,9 +39,8 @@ class OrdersController < ApplicationController
 
     respond_to do |format|
       if @order.update_attributes(params[:order])
-        @order.wizard_stage = @order.wizard_stage.to_i + 1
         flash[:notice] = 'Order was successfully updated.'
-        format.html { render :action=> "new" }
+        format.html { redirect_to(edit_production_performance_order_path(@order.performance.production, @order.performance, @order)) }
         format.xml  { head :ok }
       else
         format.html { render :action => "new" }
@@ -46,9 +48,30 @@ class OrdersController < ApplicationController
       end
     end
   end
+  
+  def show
+    @order = Order.find(params[:id])
+  end
+  
   private
   def find_parents
     @production = Production.find(params[:production_id])
     @performance = @production.performances.find(params[:performance_id])
+  end
+  
+  def find_order
+    @order = Order.find(params[:id])
+  end
+
+  def redirect_to_proper_action
+    if @order.editable?
+      if params[:action] != 'edit'
+        redirect_to(edit_production_performance_order_path(@order.performance.production, @order.performance, @order))
+      end
+    else
+      if params[:action] != 'show'
+        redirect_to(production_performance_order_path(@order.performance.production, @order.performance, @order))
+      end
+    end
   end
 end
