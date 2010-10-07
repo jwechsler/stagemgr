40098e033e3315d987efc10747d0b9869c8a6802
Tim Galeckas <tim@galeckas.com>
2010-10-07 09:00:03 -0500

    moved sorting into the database for payments and cleaned up ticket line items show

--- app/models/payments/payment.rb
+++ app/models/payments/payment.rb
@@ -1,6 +1,7 @@
 class Payment < ActiveRecord::Base
   belongs_to :order
   validates_numericality_of :amount
+  default_scope :order=>'created_at asc'
   def payment_type=(string)
     self.type=string
   end
