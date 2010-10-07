1a3c3387a8df3b1c347203cb890c6b10cf24afbf
Jeremy <jeremy@theaterwit.org>
2010-10-06 22:06:52 -0500

    fixed sort order for index

--- app/controllers/productions_controller.rb
+++ app/controllers/productions_controller.rb
@@ -13,7 +13,7 @@ class ProductionsController < ApplicationController
     @current_date = Date.today
     @b_week = Date.today.beginning_of_week
     @e_week = Date.today.end_of_week
-    @productions = Production.find(:all, :conditions=>['productions.closing_at > ? and productions.status = \'Active\'',@b_week], :order=>'case when date(productions.first_preview_at) >= date(current_date) then 1 else 0 end, case theater_id when 1 then 0 else 1 end, case when date(productions.first_preview_at) >= date(current_date) then productions.first_preview_at else productions.name end')
+    @productions = Production.find(:all, :conditions=>['productions.closing_at > ? and productions.status = \'Active\'',@b_week], :order=>'case when date(productions.first_preview_at) <= date(current_date) then 0 else 1 end, case theater_id when 1 then 0 else 1 end, case when date(productions.first_preview_at) <= date(current_date) then productions.first_preview_at else productions.name end')
     render :upcoming, :layout=>false
   end
   
