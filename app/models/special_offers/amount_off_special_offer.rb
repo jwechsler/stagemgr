3fe7782d4fa1b0c72af6c7113faa87e9a918bf1e
Jeremy Wechsler <jeremy@theaterwit.org>
2010-11-03 11:34:35 -0500

    Fixed $ off special offer to apply to all applicable tickets

--- app/models/special_offers/amount_off_special_offer.rb
+++ app/models/special_offers/amount_off_special_offer.rb
@@ -3,7 +3,7 @@ class AmountOffSpecialOffer < SpecialOffer
     # the discount will either be negative the amount configured
     # or negative the sum total of all tickets of this class
     # which ever one is smaller
-    self.amount * self.applicable_line_items(order).count * -1
+    self.amount * self.applicable_line_items(order).to_a.sum{|li| li.respond_to?(:ticket_count) ? li.ticket_count : 0} * -1
   end
   def to_s
     "#{amount} off"
