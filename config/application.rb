d54d22d05b3237fbe4a81a20251c3e06ef700c92
Jeremy Wechsler <jeremy@theaterwit.org>
2011-02-02 21:19:48 -0600

    Filtered credit card info from log (gulp)

--- config/application.rb
+++ config/application.rb
@@ -30,6 +30,11 @@ module Stagemgr
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    
+    # hide credit card parameters.
+    
+    config.filter_parameters << :password << :credit_card_number 
+    
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :enable_starttls_auto => true,
