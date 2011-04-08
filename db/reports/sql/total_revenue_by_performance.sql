403766f7130b90a56f6b0ea38b05bee318f64a20
Jeremy <jeremy@theaterwit.org>
2011-04-08 15:21:10 -0500

    New sql for order_payments view

--- db/reports/sql/total_revenue_by_performance.sql
+++ db/reports/sql/total_revenue_by_performance.sql
@@ -9,14 +9,11 @@ from (
        left outer join (ticket_classes tc) on (li.ticket_class_id = tc.id)
   where o.status in ('Fulfilled','Refunded','Processed')  and li.type = 'TicketLineItem'
   group by performance_id) order_items,  
- (select o.performance_id, p.type, round(sum(p.amount),2) gross_amount, 
-         case sum(abs(p.amount)) when 0 then 0 else round(sum(case type when 'CreditCardPayment' then case card_type when 'American Express' then 0.20 + abs(p.amount) * .052 else 0.21 + abs(p.amount)*.052 end else 0 end),2) end finance_fee 
-   from payments p left join (orders o) on (p.order_id = o.id)
-  group by o.performance_id) order_payments,
+  (select performance_id, sum(gross_amount) gross_amount, sum(finance_fee) finance_fee from order_payments group by performance_id) perf_payments,
   performances perf,
   productions prod,
   theaters t
-where perf.id = order_payments.performance_id and 
+where perf.id = perf_payments.performance_id and 
 perf.id = order_items.performance_id and
 prod.id = perf.production_id and
 perf.status != 'Inactive' and
