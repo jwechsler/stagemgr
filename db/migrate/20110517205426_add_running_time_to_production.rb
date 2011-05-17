6c16240a9ad1d622d7d97e63f38876fffca5a7ea
Jeremy Wechsler <jeremy@theaterwit.org>
2011-05-17 16:13:48 -0500

    Added running time and intermission info to productions

---
+++ db/migrate/20110517205426_add_running_time_to_production.rb
@@ -0,0 +1,11 @@
+class AddRunningTimeToProduction < ActiveRecord::Migration
+  def self.up
+    add_column :productions, :running_time, :integer
+    add_column :productions, :intermission, :boolean, {:default=>true}
+  end
+
+  def self.down
+    remove_column :productions, :running_time
+    remove_column :productions, :intermission
+  end
+end
