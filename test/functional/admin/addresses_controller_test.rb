0613793f274fe4d3e28b50d14fd6fd2b8d787a8f
Jeremy Wechsler <jeremy@theaterwit.org>
2011-05-31 15:33:41 -0500

    idea project update

--- test/functional/admin/addresses_controller_test.rb
+++ test/functional/admin/addresses_controller_test.rb
@@ -6,44 +6,61 @@ class Admin::AddressesControllerTest < ActionController::TestCase
   end
 
   test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:addresses)
+    without_access_control do
+      get :index
+      assert_response :success
+      assert_not_nil assigns(:addresses)
+    end
   end
 
   test "should get new" do
-    get :new
-    assert_response :success
+    without_access_control do
+      get :new
+      assert_response :success
+    end
+
   end
 
   test "should create admin_address" do
-    assert_difference('Address.count') do
-      post :create, :address => @address.attributes
+    without_access_control do
+      assert_difference('Address.count') do
+        post :create, :address => @address.attributes
+      end
+
+      assert_redirected_to admin_address_path(assigns(:address))
     end
 
-    assert_redirected_to admin_address_path(assigns(:admin_address))
   end
 
   test "should show admin_address" do
-    get :show, :id => @address.to_param
-    assert_response :success
+    without_access_control do
+      get :show, :id => @address.to_param
+      assert_response :success
+    end
   end
 
   test "should get edit" do
-    get :edit, :id => @address.to_param
-    assert_response :success
+    without_access_control do
+      get :edit, :id => @address.to_param
+      assert_response :success
+    end
   end
 
   test "should update admin_address" do
-    put :update, :id => @address.to_param, :admin_address => @address.attributes
-    assert_redirected_to admin_address_path(assigns(:admin_address))
+    without_access_control do
+      put :update, :id => @address.to_param, :admin_address => @address.attributes
+      assert_redirected_to admin_address_path(assigns(:admin_address))
+    end
+
   end
 
   test "should destroy admin_address" do
-    assert_difference('Address.count', -1) do
-      delete :destroy, :id => @address.to_param
+    without_access_control do
+      assert_difference('Address.count', -1) do
+        delete :destroy, :id => @address.to_param
+      end
+      assert_redirected_to admin_addresses_path
     end
 
-    assert_redirected_to admin_addresses_path
   end
 end
