387d32e80ab973dd58fb019e39682c249f2d174d
Jeremy Wechsler <jeremy@theaterwit.org>
2011-07-08 11:17:23 -0500

    Fixed email when no email present problem

--- app/models/order_tasks/outreach_task.rb
+++ app/models/order_tasks/outreach_task.rb
@@ -3,6 +3,9 @@ class OutreachTask < OrderTask
   protected
 
   def execute!
+    if self.order.address.email.blank?
+      return false
+    end
     OrderMailer.send(self.method_symbol,self.order).deliver if !self.order.address.email.nil?
     return true
   end
