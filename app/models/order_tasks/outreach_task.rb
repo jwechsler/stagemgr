a2e847e1bf101c879f2f83e28a5a7a25e161a540
Jeremy Wechsler <jeremy@theaterwit.org>
2011-07-08 13:05:10 -0500

    Don't email bad addresses forever...

--- app/models/order_tasks/outreach_task.rb
+++ app/models/order_tasks/outreach_task.rb
@@ -3,7 +3,7 @@ class OutreachTask < OrderTask
   protected
 
   def execute!
-    if self.order.address.email.blank?
+    if self.order.address.email.blank? || attempts > 2
       return false
     end
     OrderMailer.send(self.method_symbol,self.order).deliver if !self.order.address.email.nil?
