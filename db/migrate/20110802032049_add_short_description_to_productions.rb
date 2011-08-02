006584e775b681050b4e2d0cfc3adb6605943efc
Jeremy <jeremy@theaterwit.org>
2011-08-01 23:54:01 -0500

    Fixed merge problems

--- db/migrate/20110802032049_add_short_description_to_productions.rb
+++
@@ -1,9 +0,0 @@
-class AddShortDescriptionToProductions < ActiveRecord::Migration
-  def self.up
-    add_column :productions, :short_description, :string
-  end
-
-  def self.down
-    remove_column :productions, :short_description
-  end
-end
