684a9a61ea26139cf5456c03e8c31489c31fb9df
Jeremy Wechsler <jeremy@theaterwit.org>
2011-08-19 10:26:18 -0500

    Fixed exception with nil pointers on performance orders and deactivated ticket classes

--- app/controllers/production_performance_orders_controller.rb
+++ app/controllers/production_performance_orders_controller.rb
@@ -1,18 +1,24 @@
 class ProductionPerformanceOrdersController < ApplicationController
   def new
     @production = Production.find(params[:production_id])
-    @performance = @production.performances.find(params[:performance_id])
-    @available_ticket_classes = @performance.ticket_class_allocations.select{|tca|tca.available}.map{|tca|tca.ticket_class}.select{|tc|tc.web_visible}
-    @order = @performance.orders.build(:status=>Order::WEB)
-    @order.status = Order::NEW
-    @order.address = Address.new
-    @available_ticket_classes.each{|tc|@order.ticket_line_items.build(:ticket_class=>tc)}
-    # @order.donation_line_items.build(:donation_amount=>0)
-    @order_for_to_s = @production.name + ' on ' + @performance.performance_date.to_formatted_s(:long_ordinal) +
-                      ' at ' + @performance.performance_time.to_formatted_s(:hour_min)
-  
-    respond_to do |format|
-      format.html{ render '/orders/edit', :layout=>'ext_site_wrapper' }
+    @performance = @production.performances.find(params[:performance_id]) unless @production.nil?
+    if @production.nil? || @performance.nil? || @performance.inactive?
+      respond_to do |format|
+        format.html { render '/orders/not_available', :layout=>'ext_site_wrapper'}
+      end
+    else
+      @available_ticket_classes = @performance.ticket_class_allocations.select { |tca| tca.available }.map { |tca| tca.ticket_class }.select { |tc| tc.web_visible unless tc.nil? }
+      @order = @performance.orders.build(:status=>Order::WEB)
+      @order.status = Order::NEW
+      @order.address = Address.new
+      @available_ticket_classes.each { |tc| @order.ticket_line_items.build(:ticket_class=>tc) }
+      # @order.donation_line_items.build(:donation_amount=>0)
+      @order_for_to_s = @production.name + ' on ' + @performance.performance_date.to_formatted_s(:long_ordinal) +
+          ' at ' + @performance.performance_time.to_formatted_s(:hour_min)
+
+      respond_to do |format|
+        format.html { render '/orders/edit', :layout=>'ext_site_wrapper' }
+      end
     end
   end
 end
