3f4d62e20acc6f834c548e78dfeb6496db34d759
Jeremy Wechsler <jeremy@theaterwit.org>
2011-09-21 23:15:17 -0500

    Don't forget membership tasks

--- db/migrate/20110922031724_fix_membership_recurring_payments.rb
+++ db/migrate/20110922031724_fix_membership_recurring_payments.rb
@@ -1,9 +1,12 @@
 class FixMembershipRecurringPayments < ActiveRecord::Migration
   def self.up
     memberships = Membership.all
-    memberships.each {|m| m.update_from_profile! }
 
     memberships.each do |m|
+      m.update_from_profile!
+      o = m.membership_line_item.order
+      o.tasks << CheckMembershipTask.new(:execute_at=>m.next_billing_date + 2.hours)
+      o.save!
       c_start_date = m.member_since
       o_id = m.membership_line_item.order_id
       m.number_cycles_completed.times do
@@ -12,6 +15,8 @@ class FixMembershipRecurringPayments < ActiveRecord::Migration
       end
     end
     execute "commit"
+
+
   end
 
   def self.down
