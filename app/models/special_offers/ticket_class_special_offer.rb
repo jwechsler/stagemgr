a6f5c9f087d99609fd2f850159dbbea35d75a240
Jeremy Wechsler <jeremy@theaterwit.org>
2011-10-09 23:35:52 -0500

    Ticket Class special offer requires ticket_class_code

--- app/models/special_offers/ticket_class_special_offer.rb
+++ app/models/special_offers/ticket_class_special_offer.rb
@@ -1,4 +1,7 @@
 class TicketClassSpecialOffer < SpecialOffer
+
+  validates_presence_of :use_ticket_class_code
+
   def calculate_discount(order)
     self.applicable_line_items(order).map{|li|li.total}.sum * self.amount/-100
   end
