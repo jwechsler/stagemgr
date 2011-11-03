981fa86a03e52be7f7a70832eb3ef49d197d87f1
Jeremy Wechsler <jeremy@theaterwit.org>
2011-11-03 13:37:31 -0500

    Fixed ticket class order processing

--- app/models/special_offers/ticket_class_special_offer.rb
+++ app/models/special_offers/ticket_class_special_offer.rb
@@ -3,8 +3,18 @@ class TicketClassSpecialOffer < SpecialOffer
   def apply_to_order(order)
     ticket_class = TicketClass.where("production_id = ? and class_code = ?",order.performance.production.id,self.change_ticket_class_code).first
     raise "Non applicable offer to this order" if ticket_class.nil?
-    ticket_line_items = self.applicable_line_items(order)
-    ticket_line_items.each {|li| li.ticket_class = ticket_class }
+    ticket_line_ids = self.applicable_line_items(order).map{|l| l.id}
+
+    new_items = Array.new
+    order.ticket_line_items.select{|li|  ticket_line_ids.include?(li.id)}.each do |li|
+      new_item = TicketLineItem.new(:order_id=>order.id,
+                                    :ticket_class=>ticket_class,
+                                    :ticket_count => li.ticket_count,
+                                    :price_override => li.price_override)
+      new_items << new_item
+      order.ticket_line_items << new_item
+      order.ticket_line_items.delete(li)
+    end
     self
   end
 
