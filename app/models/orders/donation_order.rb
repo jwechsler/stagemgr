ddbfe37d01b4ae80f7935a8718cc2d06a6d4c633
Jeremy Wechsler <jeremy@theaterwit.org>
2012-03-02 10:58:49 -0600

    Update donation sync, new survey link

--- app/models/orders/donation_order.rb
+++ app/models/orders/donation_order.rb
@@ -36,13 +36,13 @@ class DonationOrder < Order
 
   def sync_to_salesforce!(sf_user = nil, sf_donationtype = nil)
     if self.finalized?
-      c = self.address.sync_to_salesforce!
+      c = self.address.sf
       account = Salesforce::Account.find_by_npe01__One2OneContact__c(c.Id)
 
       donation = Salesforce::Opportunity.find_by_stagemgr_id__c(self.id)
       if donation.nil?
         donation = Salesforce::Opportunity.create("Probability"=>100.0, "StageName"=>"Posted",
-                                                  "Name"=>'Online donation', "Amount"=>self.total,
+                                                  "Name"=>"#{self.address.full_name} (Online)", "Amount"=>self.total,
                                                   "CloseDate"=>self.last_processed_on,
                                                   "AccountId"=>account.Id,
                                                   "npe01__Contact_Id_for_Role__c"=>account.Id,
