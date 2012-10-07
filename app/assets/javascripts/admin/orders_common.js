6a4366e45406c2f626370171bf7bc3c8e3ea5860
Jeremy Wechsler <jeremy@theaterwit.org>
2012-10-07 16:33:02 -0500

    Included front end order common refactoring

---
+++ app/assets/javascripts/admin/orders_common.js
@@ -0,0 +1,10 @@
+//= require utility
+//= require orders/payments
+//= require admin/address_utility
+//= require admin/ticket_orders/CardReader
+//= require_self
+
+
+var order_type = 'ticket_order';
+
+setup_payment_form(order_type);
