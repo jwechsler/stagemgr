c33132664bbd719e161b834a30d308e22c84f245
Jeremy Wechsler <jeremy@theaterwit.org>
2012-10-12 13:31:43 -0500

    Fixed bad js in show special seating jquery js

--- app/assets/javascripts/ticket_orders.js
+++ app/assets/javascripts/ticket_orders.js
@@ -7,7 +7,7 @@ var order_type = 'ticket_order'
 setup_payment_form(order_type);
 
 function show_special_request_form(order_type) {
-  e = ${"#" + order_type + "_special_request"}
+  e = $("#" + order_type + "_special_request")
   e.fadeToggle();
   if !e.is(':visible') {
     e.val('')
