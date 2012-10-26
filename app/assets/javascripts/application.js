8bb1d56ffa8ab5e67f292074c5a8195c9e294aa2
Jeremy Wechsler <jeremy@theaterwit.org>
2012-10-25 22:24:45 -0500

    Fixed frontend and common references

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,16 @@
 //= require jquery
 //= require jquery-ui
 //= require jquery_ujs
+//= require_this
+
+$(function(){
+  $('.performance_list a[data-type=html]').on('ajax:success', function(event, data, status, xhr) {
+    $('#production_detail').html(data)
+  });
+
+  $('a.ajax_target_loader').click(function() {
+
+    $("#" + $(this).attr('target_id')).load($(this).attr('fetch_url'), "id=" + $(this).attr('fetch_id'))
+
+  })
+})
