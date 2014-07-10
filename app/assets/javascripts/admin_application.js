92e387a8e5deaa335c9dd84cd4af43949908a99c
Jeremy Wechsler <jeremy@theaterwit.org>
2014-07-10 00:11:59 -0500

    Updated Theaters Admin Pages

--- app/assets/javascripts/admin_application.js
+++ app/assets/javascripts/admin_application.js
@@ -1,3 +1,31 @@
 //= require application
 //= require admin/orders_common
 
+
+// setup dataTable with standard active switch selector. Call from ready state
+function setupActiveSwitchOnDataTable(table_selector, status_column_idx) {
+  $("div.toolbar-buttons").html('<a style="margin-right:6px; width:10em;" id="active-switch" href="#" class="tiny button right" >Active</a>');
+    var settings = $(table_selector).dataTable().fnSettings();
+    var activeSwitch = $('#active-switch');
+    var table = $(table_selector).DataTable();
+    if ((typeof settings.aoPreSearchCols != 'undefined') && settings.aoPreSearchCols[status_column_idx].sSearch.length==0) {
+      activeSwitch.text('Show Active')
+    } else {
+      activeSwitch.text('Show All')
+    }
+    activeSwitch.prop('checked',true);
+    activeSwitch.on('click', function () {
+      cSearch = settings.aoPreSearchCols[status_column_idx].sSearch
+      if (cSearch.length==0) {
+        find_text = "^Active$";
+        $(this).text('Show All')
+      } else {
+        find_text = ""
+        $(this).text('Show Active')
+      }
+      table.column(status_column_idx)
+        .search( find_text , true, false )
+        .draw();
+
+    });
+}
