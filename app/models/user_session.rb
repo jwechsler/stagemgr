d185ec104e75a3382368fb0726a3ef112fe54792
Jeremy Wechsler <jeremy@theaterwit.org>
2014-08-28 00:19:14 -0500

    Fixed nil error A NoMethodError occurred in user_sessions#create:
      undefined method `status' for nil:NilClass
      app/models/user_session.rb:7:in `is_active?'

--- app/models/user_session.rb
+++ app/models/user_session.rb
@@ -4,6 +4,7 @@ class UserSession < Authlogic::Session::Base
 
   private
   def is_active?
+    errors.add(:login,'Session expired') if self.attempted_record.nil?
     errors.add(:login, "#{self.attempted_record.email} is currently inactive") if self.attempted_record.status.eql? User::INACTIVE
   end
 
