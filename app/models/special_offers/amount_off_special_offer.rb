c482f985fa26cd1446b643c1154e0bc83a39a244
Jeremy Wechsler <jeremy@theaterwit.org>
2014-09-06 16:43:44 -0500

    Fixed nil error for some (broken) special offers
    A NoMethodError occurred in orders#index:
    
      undefined method `*' for nil:NilClass
      app/models/special_offers/amount_off_special_offer.rb:6:in `calculate_discount'

--- app/models/special_offers/amount_off_special_offer.rb
+++ app/models/special_offers/amount_off_special_offer.rb
@@ -3,7 +3,7 @@ class AmountOffSpecialOffer < SpecialOffer
     # the discount will either be negative the amount configured
     # or negative the sum total of all tickets of this class
     # which ever one is smaller
-    self.amount * self.applicable_line_items(order).to_a.sum{|li| li.respond_to?(:ticket_count) ? li.ticket_count : 0} * -1
+    (self.amount || 0) * (self.applicable_line_items(order).to_a.sum{|li| li.respond_to?(:ticket_count) ? li.ticket_count : 0} || 0) * -1
   end
 
   def description(order)
