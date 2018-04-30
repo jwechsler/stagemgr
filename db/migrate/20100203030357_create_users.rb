9c07b17ad1552c7c7da256f9b443bff0f560fe81
Jeremy Wechsler <jeremy@theaterwit.org>
2018-04-30 14:30:31 -0500

    Create a default user when initializing a new database

--- db/migrate/20100203030357_create_users.rb
+++ db/migrate/20100203030357_create_users.rb
@@ -7,10 +7,10 @@ class CreateUsers < ActiveRecord::Migration
       t.string    :persistence_token,   :null => false                # required
       t.string    :single_access_token, :null => false                # optional, see Authlogic::Session::Params
       t.string    :perishable_token,    :null => false                # optional, see Authlogic::Session::Perishability
-      
+
       t.boolean   :is_administrator,    :null => false
       t.boolean   :is_box_office_user,  :null => false
-      
+
 
       # Magic columns, just like ActiveRecord's created_at and updated_at. These are automatically maintained by Authlogic if they are present.
       t.integer   :login_count,         :null => false, :default => 0 # optional, see Authlogic::Session::MagicColumns
@@ -27,7 +27,10 @@ class CreateUsers < ActiveRecord::Migration
     # Add indexes
     #
     add_index :users, :perishable_token
-    add_index :users, :email    
+    add_index :users, :email
+
+    User.create(email:"youremail@yourtheater.com",is_administrator:true,password:"changemeimmediately",password_confirmation:"changemeimmediately")
+
   end
 
   def self.down
