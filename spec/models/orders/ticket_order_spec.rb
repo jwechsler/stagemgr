7059bed5ca860893fdd8e5a86b624cb73c8e5203
Jeremy Wechsler <jeremy@theaterwit.org>
2018-05-07 22:40:14 -0500

    Removed deprecated syntax for rspec

--- spec/models/orders/ticket_order_spec.rb
+++ spec/models/orders/ticket_order_spec.rb
@@ -34,7 +34,7 @@ describe "a ticket order" do
   it "should mark its holder has having attended the production when fulfilled" do
     original_order = FactoryBot.create(:ticket_order_for_a_pair_of_tickets_paid_with_cash)
     original_order.transition_to!(Order::FULFILLED)
-    original_order.performance.production.attendees.count.should == 1
+    expect(original_order.performance.production.attendees.size).to eq(1)
   end
 
   it "should unmark the holder has having attended when refunded" do
@@ -83,12 +83,12 @@ describe "a ticket order" do
     o2.performance.production.save!
 
     o2.performance.reload
-    o2.performance.number_of_seats_left.should == 6
+    expect(o2.performance.number_of_seats_left).to eq(6)
     o2.transition_to!(Order::FULFILLED)
-    o2.performance.number_of_seats_left.should == 6
+    expect(o2.performance.number_of_seats_left).to eq(6)
     o2.transition_to!(Order::UNCLAIMED)
     o2.performance.reload
-    o2.performance.number_of_seats_left.should == 8
+    expect(o2.performance.number_of_seats_left).to eq(8)
   end
 
   context "when overselling" do
@@ -103,14 +103,14 @@ describe "a ticket order" do
     it "cannot processes if it would oversell a particular ticket class" do
       production = FactoryBot.create(:production, :capacity=>4)
       performance = FactoryBot.create(:performance, :production=>production)
-      performance.number_of_seats_left.should eq(4)
+      expect(performance.number_of_seats_left).to eq(4)
       o = FactoryBot.create(:ticket_order_for_a_pair_of_tickets_paid_with_cash, :performance=>performance)
       o2 = FactoryBot.create(:ticket_order_for_a_pair_of_tickets_paid_with_cash, :performance=>performance)
-      performance.number_of_seats_left.should eq(0)
+      expect(performance.number_of_seats_left).to eq(0)
 
-      lambda { order = FactoryBot.create(:ticket_order_for_a_pair_of_tickets, :performance=>performance )
+      expect(lambda { order = FactoryBot.create(:ticket_order_for_a_pair_of_tickets, :performance=>performance )
                   order.transition_to!(Order::PROCESSING)
-            }.should raise_error(ActiveRecord::RecordInvalid)
+            }).to raise_error(ActiveRecord::RecordInvalid)
     end
 
     it "cannot processes if it would oversell a performance" do
@@ -122,10 +122,10 @@ describe "a ticket order" do
       production.reload
       performance = FactoryBot.create(:performance, :production=>production)
 
-      performance.number_of_seats_left.should eq(2)
-      tc_1.number_left(performance).should eq(2)
-      tc_2.number_left(performance).should eq(2)
-      tc_3.number_left(performance).should eq(2)
+      expect(performance.number_of_seats_left).to eq(2)
+      expect(tc_1.number_left(performance)).to eq(2)
+      expect(tc_2.number_left(performance)).to eq(2)
+      expect(tc_3.number_left(performance)).to eq(2)
 
       o  = FactoryBot.create(:ticket_order, :performance=>performance)
       o.ticket_line_items << FactoryBot.build(
@@ -133,28 +133,28 @@ describe "a ticket order" do
                                   :ticket_class=>tc_1,
                                   :ticket_count=>1,
                                   :order=>o)
-      tc_1.number_left(performance).should eq(1)
-      tc_2.number_left(performance).should eq(1)
-      tc_2.number_left(performance).should eq(1)
+      expect(tc_1.number_left(performance)).to eq(1)
+      expect(tc_2.number_left(performance)).to eq(1)
+      expect(tc_2.number_left(performance)).to eq(1)
 
       o.ticket_line_items << FactoryBot.build(
                                   :ticket_line_item,
                                   :ticket_class=>tc_2,
                                   :ticket_count=>2,
                                   :order=>o)
-      tc_1.number_left(performance).should eq(-1)
-      tc_2.number_left(performance).should eq(-1)
-      tc_2.number_left(performance).should eq(-1)
+      expect(tc_1.number_left(performance)).to eq(-1)
+      expect(tc_2.number_left(performance)).to eq(-1)
+      expect(tc_2.number_left(performance)).to eq(-1)
 
       #o.ticket_line_items << FactoryBot.create(
       #                            :ticket_line_item,
       #                            :ticket_class=>tc_3,
       #                            :ticket_count=>1,
       #                            :order=>o)
-      o.number_of_seats.should eq(3)
+      expect(o.number_of_seats).to eq(3)
 
 
-      lambda { o.transition_to!(Order::PROCESSING) }.should raise_error(ActiveRecord::RecordInvalid)
+      expect(lambda { o.transition_to!(Order::PROCESSING) }).to raise_error(ActiveRecord::RecordInvalid)
     end
 
     it "can mark an order in a sold-out performance as unclaimed" do
@@ -164,23 +164,23 @@ describe "a ticket order" do
       o2 = FactoryBot.create(:ticket_order_for_a_pair_of_tickets_paid_with_cash, :performance=>performance)
       o.transition_to!(Order::FULFILLED)
       o2.transition_to!(Order::FULFILLED)
-      performance.number_of_seats_left.should eq(0)
+      expect(performance.number_of_seats_left).to eq(0)
       o.transition_to!(Order::UNCLAIMED)
-      performance.number_of_seats_left.should eq(2)
+      expect(performance.number_of_seats_left).to eq(2)
     end
 
     it "creates tasks for asynchronous post-operation, except where prohibited by the payment type" do
       o = FactoryBot.create(:ticket_order_for_a_pair_of_tickets_paid_with_cash)
       task_count = o.tasks.count
-      task_count.should be > 0
+      expect(task_count).to be > 0
       payment_type = o.payment_type
       payment_type.order_task_suppressions << FactoryBot.create(:order_task_suppression, task_type:o.tasks.first.type, method_name:o.tasks.first.method_symbol)
       payment_type.save
       o2 = FactoryBot.create(:ticket_order_for_a_pair_of_tickets_paid_with_cash)
-      o2.tasks.count.should eq(task_count)
+      expect(o2.tasks.count).to eq(task_count)
       t = o2.tasks.first
       t.run!
-      t.status.should eq('Cancelled')
+      expect(t.status).to eq('Cancelled')
 
     end
 
@@ -214,7 +214,7 @@ describe "a ticket order" do
 
     it "does not send an automatic followup" do
       followups = @ticket_order.tasks.select{|t| t.method_symbol.include?('followup')}
-      followups.count.should == 0
+      expect(followups.count).to eq(0)
     end
 
   end
