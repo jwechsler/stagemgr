ab90861296eeaca515253441ac1d9879fed0a191
Jeremy Wechsler <jeremy@theaterwit.org>
2018-08-30 10:31:56 -0500

    removed previous verificaton hack for non-ssl connections in ActiveResource

--- app/models/tktprint/print_order.rb
+++ app/models/tktprint/print_order.rb
@@ -2,6 +2,6 @@ class PrintOrder < ActiveResource::Base
   self.site = $TKTPRINT['service']
   self.format = :xml
   self.element_name = "order"
-  self.ssl_options={:verify_mode=>OpenSSL::SSL::VERIFY_NONE}
+  # self.ssl_options={:verify_mode=>OpenSSL::SSL::VERIFY_NONE}
   self.timeout = 120
 end
