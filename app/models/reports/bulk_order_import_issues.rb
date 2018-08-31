453b6845d2238f7dba0d8e148103c0604e60ee3a
Jeremy Wechsler <jeremy@theaterwit.org>
2018-08-31 16:46:12 -0500

    WIP

--- app/models/reports/bulk_order_import_issues.rb
+++ app/models/reports/bulk_order_import_issues.rb
@@ -5,11 +5,8 @@ class BulkOrderImportIssues < Report
     @headers += [:Id,:Name,:PerformanceCode,:Seating,:TicketClass]
   end
 
-  def create(issues_list)
-    CSV
-    self.data << issue.join(',')
-    file_name = "/tmp/import_issues_#{reporting_user_id}.csv"
-    self.save_report_to_filestore(file_name, issues_list.)
+  def append_problem(row, message)
+  def create()
 
   end
 end
