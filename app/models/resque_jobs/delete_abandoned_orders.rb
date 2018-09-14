502d72b64aaf1342c47ef375fc6c0e2fc710e6dc
Jeremy Wechsler <jeremy@theaterwit.org>
2018-09-14 13:38:40 -0500

    Log errors from destroy instead of abandoning cleanup

--- app/models/resque_jobs/delete_abandoned_orders.rb
+++ app/models/resque_jobs/delete_abandoned_orders.rb
@@ -5,7 +5,13 @@ class DeleteAbandonedOrders
     orders = Order.where("status = :status and updated_at < :time_window",
       status: Order::PROCESSING, time_window: Time.now-8.minutes)
     orders.each do |o|
-      o.destroy
+      begin
+        o.destroy
+      rescue RuntimeError => e
+        Rails.logger.error("Could not delete abandonded order #{o.id}:")
+        Rails.logger.error e.message
+        e.backtrace.each { |line| Rails.logger.error line }
+      end
     end
   end
 
