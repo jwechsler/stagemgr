320f3dda782827ce07a4cf4fc63654bb82671939
Jeremy Wechsler <jeremy@theaterwit.org>
2019-01-16 17:03:19 -0600

    Fixed assign seats old value for ticket classes

--- app/models/reports/house_management_report.rb
+++ app/models/reports/house_management_report.rb
@@ -29,7 +29,7 @@ class HouseManagementReport < Report
     orders.each do |o|
       seat_assignments = o.seat_assignments
       print_tags = house_tags(o.address)
-      if !o.special_request.blank? || !o.notes.blank? || o.address.is_current_member? || o.address.is_donor? || !print_tags.empty?
+      if !o.special_request.blank? || !o.notes.blank? || o.address.is_current_member? || o.address.is_donor? || !print_tags.empty? || o.assigned_seats?
         note_column = o.notes.blank? ? "" : o.notes
         unless print_tags.empty?
           note_column += "<br/>" unless note_column.blank?
