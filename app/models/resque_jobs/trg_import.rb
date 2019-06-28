e085028e2ff359f2f0e68c60dd0d96f401cae0fd
Jeremy Wechsler <jeremy@theaterwit.org>
2019-06-28 14:07:21 -0500

    Updated headers for TRG Import

--- app/models/resque_jobs/trg_import.rb
+++ app/models/resque_jobs/trg_import.rb
@@ -13,12 +13,13 @@ class TrgImport
       last_name_idx = 0
       prefix_idx = 0
       full_name_idx = 0
-      address2_idx = 0
+      address1_idx = 0
       city_idx = 0
       state_idx = 0
       zip_idx = 0
       zip4_idx = 0
       email1_idx = 0
+      phone_idx = 0
       filestore = FileStore.find(filestore_id)
       production = Production.find(production_id) unless production_id == 0
       filestore.notes = "Importing #{production.nil? ? '' : production.name + ' '}attendees..."
@@ -27,28 +28,26 @@ class TrgImport
         if headers.nil? then
           _index = 0
           headers = Hash[row.map {|header| _index += 1; [header, _index]}]
-          ['FirstName','MiddleName','LastName','Prefix','FullName','Address','City','StateCode','PostalCode','Zip4','EmailAddress1'].each do |t|
+          ['FirstName','LastName','Prefix','FullName','Address','City','StateCode','PostalCode','Zip4','EmailAddress1','HomePhone'].each do |t|
             raise "Missing expected header #{t}" if headers[t].nil?
           end
-
           first_name_idx = headers['FirstName'] - 1
-          middle_name_idx = headers['MiddleName'] - 1
           last_name_idx = headers['LastName'] - 1
           prefix_idx = headers['Prefix'] - 1
           full_name_idx = headers['FullName'] - 1
-          address2_idx = headers['Address'] - 1
+          address1_idx = headers['Address'] - 1
           city_idx = headers['City'] - 1
           state_idx = headers['StateCode'] - 1
           zip_idx = headers['PostalCode'] - 1
           zip4_idx = headers['Zip4'] - 1
           email1_idx = headers['EmailAddress1'] - 1
+          phone_idx = headers['HomePhone'] - 1
           else
           total += 1
 
           unless row[last_name_idx].blank? && row[full_name_idx].blank?
             a = Address.new if a.nil?
             a.first_name = row[first_name_idx]
-            a.middle_name = row[middle_name_idx]
             a.last_name = row[last_name_idx]
             if row[full_name_idx].blank?
               a.full_name = a.first_name unless a.first_name.blank?
@@ -58,10 +57,11 @@ class TrgImport
               a.full_name = row[full_name_idx]
             end
             a.prefix = row[prefix_idx]
-            a.line1 = row[address2_idx]
+            a.line1 = row[address1_idx]
             a.city = row[city_idx]
             a.state = row[state_idx]
             a.zipcode = row[zip_idx]
+            a.phone = row[phone_idx]
             a.zipcode += '-' + row[zip4_idx] unless row[zip4_idx].blank?
             a.email = row[email1_idx] if a.email.blank?
             merge_check = a.find_original
