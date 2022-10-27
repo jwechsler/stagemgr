e72aabd0a942a3fc1801644d5261845145cfd113
Jeremy Wechsler <jeremy@theaterwit.org>
2022-10-27 09:08:26 -0700

    Fixed introduced bug where orders didn't total correctly and the price was modifiable under certain circumstances for Fixed ticket classses

--- app/assets/javascripts/orders_common.js
+++ app/assets/javascripts/orders_common.js
@@ -1,6 +1,52 @@
 //= require orders/payments
 //= require_this
 
+function recalculate_ga_total() {
+  sum = 0.00
+  $(".total").each(function() {
+    sum += parseFloat($(this).text().replace("$",""))
+
+  });
+  var formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  });
+  $('#order_total').text(formatter.format(sum));
+}
+
+function recalculate_row_total(order_type, row) {
+  var tds = row.children('.cell');
+  var performance_code_input = jQuery('#' + order_type + '_performance_code');
+  var ticket_class_code_input = tds.find('input.code-input');
+  var price_input = tds.find('input.price');
+  var ticket_class_code = '';
+  var ticket_type = tds.find('input.ticket_types').val();
+  line_price = 0;
+  var performance_code;
+  ticket_class_code = ticket_class_code_input.val();
+
+  if (ticket_class_code.length > 0) {
+    var price_override_input = row.find('input.price');
+    if (ticket_type != 'Donation') {
+      price_override_input.attr('disabled', 'disabled');
+      // price_override_input.val('');
+    } else {
+      price_override_input.removeAttr('disabled');
+    }
+    var ticket_count = tds.find('input.ticket_count').val();
+    var ticket_price = tds.find('input.price').val() || row.attr('price');
+    var line_price = 1 * ticket_count * ticket_price;
+  }
+  row.find('div.total').text(formatCurrency(line_price));
+  recalculate_ga_total()
+}
+
+function recalculate_all_row_totals(order_type) {
+  jQuery('.line_item').each(function() {
+    recalculate_row_total(order_type,this);
+  });
+}
+
 function show_gift_form() {
   e = $("#gift_information");
   v = $(".third_party_checkbox input.boolean").each(function() {
