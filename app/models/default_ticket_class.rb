c161ecf2c08aeb7c8b8ceb1484cc3eea5abcfc19
Jeremy Wechsler <jeremy@theaterwit.org>
2022-11-20 00:47:18 -0600

    Updated error messages for default ticket classes

--- app/models/default_ticket_class.rb
+++ app/models/default_ticket_class.rb
@@ -4,6 +4,8 @@ class DefaultTicketClass < ApplicationRecord
   validates_presence_of :ticket_price
   validates_presence_of :class_name
   validates_presence_of :ticketing_fee
+  validates_numericality_of :ticket_price
+  validates_numericality_of :ticketing_fee
   validates_inclusion_of :ticket_type, in: TicketClass::TICKET_TYPES, message: 'Invalid ticket type'
 
   def to_hash
