8dc18df6df1aa2ab9b47fd50ee099bda9e387096
Jeremy Wechsler <jeremy@theaterwit.org>
2022-11-25 08:21:30 -0600

    Revert "Updated checkout for gitignore directives"
    This reverts commit 09e7a7fe3b52602430eaec54dc1ba6002a4c7069.

---
+++ config/initializers/active_merchant.rb
@@ -0,0 +1,15 @@
+require 'active_merchant'
+
+# Use the below for authorize.net
+#is the api key and transaction id for a authorize.net test account?
+#ActiveMerchant::Billing::Base.mode = :production
+#ACTIVE_MERCHANT_LOGIN='5V4s5jT2'
+#ACTIVE_MERCHANT_PASSWORD='9TC49d2x4n6k4GNz'
+##should the requests be made in test mode (can run test against a real server)
+#if ENV['RAILS_ENV'] == 'production'
+#  ACTIVE_MERCHANT_TEST_MODE=false
+#else
+#  ACTIVE_MERCHANT_TEST_MODE=true
+#end
+#
+
