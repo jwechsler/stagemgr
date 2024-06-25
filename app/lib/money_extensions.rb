d59a878ad6fe7cf04f947a0304620650c5648d48
Jeremy Wechsler <jeremy@theaterwit.org>
2024-06-25 12:00:14 -0700

    Moved money code into initializer for clarity

--- app/lib/money_extensions.rb
+++
@@ -1,9 +0,0 @@
-# lib/money_extensions.rb
-
-Money.class_eval do
-  def formatted_with_symbol
-    a,b = sprintf("%0.2f", amount).split('.')
-    a.gsub!(/(\d)(?=(\d{3})+(?!\d))/, '\\1,')
-    "#{symbol}#{a}.#{b}"
-  end
-end
--- config/initializers/money_extensions_loader.rb
+++ config/initializers/money_extensions_loader.rb
@@ -1,3 +1,11 @@
-# config/initializers/money_extensions_loader.rb
+# config/initializers/money_extensions.rb
 
-require Rails.root.join('app/lib/money_extensions')
+# avoid weird number_to_currency bug
+
+Money.class_eval do
+  def formatted_with_symbol
+    a,b = sprintf("%0.2f", amount).split('.')
+    a.gsub!(/(\d)(?=(\d{3})+(?!\d))/, '\\1,')
+    "#{symbol}#{a}.#{b}"
+  end
+end
